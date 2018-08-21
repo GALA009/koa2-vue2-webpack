37c42a126eba72e7a4695c1bf5d4a8292a03c870
Zhang Genmao <zhanggenmao@yingzi.com>
2018-08-21 10:54:14 +0800

    up

--- src/components/create.vue
+++ src/components/create.vue
@@ -2,33 +2,36 @@
   <div>
     <Row>
       <Col span="20" offset="2">
-        <Form :model="formData" :rules="ruleInline" label-position="top">
+        <Form :model="formData" :rules="ruleInline" label-position="left">
           <FormItem label="统计名称">
-            <Input type="text" v-model="formData.username" placeholder="请输入统计名称" :maxlength=100 clearable />
+            <Input type="text" v-model="formData.title" placeholder="请输入统计名称" :maxlength=100 clearable />
           </FormItem>
-          <FormItem label="日期">
-            <DatePicker type="date" placeholder="请选择日期" style="width:100%"></DatePicker>
+          <FormItem label="开始时间">
+            <DatePicker v-model="formData.startTime" type="datetime" format="yyyy-MM-dd HH:mm" placeholder="请选择开始时间" style="width:100%"></DatePicker>
+          </FormItem>
+          <FormItem label="结束时间">
+            <DatePicker v-model="formData.endTime" type="datetime" format="yyyy-MM-dd HH:mm" placeholder="请选择结束时间" style="width:100%"></DatePicker>
           </FormItem>
           <!-- 单选开始 -->
-          <FormItem label="标题">
+          <FormItem label="标题" v-for="(item, index) in formData.topicData" :key="index">
             <div class="form-item">
-              <Input type="text" style="width:60%" v-model="formData.topicData" placeholder="请输入标题名称" :maxlength=100 clearable />
-              <Button type="text">删除该项</Button>
+              <Input type="text" style="width:60%" v-model="item.title" placeholder="请输入标题名称" :maxlength=100 clearable />
+              <Button type="text" @click="deleteItem(index)">删除该项</Button>
             </div>
-            <div class="form-item">
-              <Radio class="fl" v-model="single"   disabled>
-                <Input type="text" v-model="formData.topicData" :maxlength=100 clearable />
+            <div class="form-item" v-if="item.type !== 3" v-for="(subItem, subIndex) in item.valueList" :key="subIndex">
+              <Radio class="fl" disabled>
+                <Input type="text" v-model="subItem.value" :maxlength=100 clearable />
               </Radio>
               <Icon type="ios-close" size="24"/>
             </div>
-            <div class="form-item">
+            <div class="form-item" v-if="item.type !== 3">
               <Icon type="ios-add-circle-outline" size="24"/>新增参数
             </div>
           </FormItem>
           <!-- 单选结束 -->
 
           <!-- 多选开始 -->
-          <FormItem label="标题">
+          <!-- <FormItem label="标题">
             <Input type="text" v-model="formData.topicData" placeholder="请输入标题名称" :maxlength=100 clearable />
             <div class="form-item">
               <Radio class="fl" v-model="single" style="width:80%"  disabled>
@@ -37,13 +40,13 @@
               <Icon type="ios-close" size="24"/>
             </div>
             <div class="form-item">
-              <Icon type="ios-add-circle-outline" size="24"/>新增参数
+              <Button type="text" icon="ios-add-circle-outline">新增参数</Button>
             </div>
-          </FormItem>
+          </FormItem> -->
           <!-- 多选结束 -->
 
           <!-- 输入框开始 -->
-          <FormItem label="标题">
+          <!-- <FormItem label="标题">
             <Input type="text" v-model="formData.topicData" placeholder="请输入标题名称" :maxlength=100 clearable />
             <div class="form-item">
               <Radio class="fl" v-model="single" style="width:80%"  disabled>
@@ -51,19 +54,19 @@
               </Radio>
               <Icon type="ios-close" size="24"/>
             </div>
-          </FormItem>
+          </FormItem> -->
           <!-- 输入框结束 -->
 
           <!-- 题目类型开始 -->
           <FormItem label="类型">
-            <Select v-model="model8">
+            <Select v-model="itemType">
               <Option :value="1">单选</Option>
               <Option :value="2">多选</Option>
               <Option :value="3">输入框</Option>
             </Select>
           </FormItem>
           <FormItem>
-            <Button type="primary" @click="submit" style="width:100%">新增题目</Button>
+            <Button type="primary" @click="addItem" style="width:100%">新增题目</Button>
           </FormItem>
           <!-- 题目类型结束 -->
           <FormItem>
@@ -90,20 +93,22 @@ export default {
   name: 'register',
   data () {
     return {
+      itemType: 1, // 题目类型，1：单选，2:多选，3:输入框，默认1
       formData: {
-        user: '', // 用户名
-        passwd: '', // 密码
-        topicData: [] // 题目对象
+        title: '', // 名称
+        startTime: '', // 开始时间
+        endTime: '', // 结束时间
+        topicData: [] // 题目
       },
       ruleInline: {
-        username: [
-          {required: true, message: '请输入姓名'}
+        title: [
+          {required: true, message: '请输入名称'}
         ],
-        mobile: [
-          {required: true, message: '请输入手机号'}
+        startTime: [
+          {required: true, message: '请选择开始时间'}
         ],
-        passwd: [
-          {required: true, message: '请输入密码'}
+        endTime: [
+          {required: true, message: '请选择结束时间'}
         ]
       }
     };
@@ -123,8 +128,32 @@ export default {
     }
   },
   mounted () {
+    console.log('topicData', this.formData.topicData);
   },
   methods: {
+    // 新增项目
+    addItem() {
+      if(this.itemType=== 1 || this.itemType===2) {
+        this.formData.topicData.push({
+          type: this.itemType,
+          title: '',
+          valueList: [{
+            value: ''
+          }]
+        });
+      }
+      else {
+        this.formData.topicData.push({
+          type: this.itemType,
+          title: '',
+        });
+      }
+    },
+    // 删除项目
+    deleteItem(index) {
+      console.log('index', index);
+      this.formData.topicData.splice(index, 1);
+    },
     get () {
       let that = this;
 
@@ -161,17 +190,31 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
-  .fl {
-    float: left;
-    &::after {
-      clear: both;
-      height: 0;
-      content: '';
-      overflow: hidden;
+.ivu-form {
+  .ivu-form-item {
+    .ivu-form-item-label {
+      float: left;
+      &::after {
+        clear: both;
+        height: 0;
+        content: '';
+        overflow: hidden;
+      }
     }
   }
+}
 
-  .form-item {
-    margin-top: 6px;
+.fl {
+  float: left;
+  &::after {
+    clear: both;
+    height: 0;
+    content: '';
+    overflow: hidden;
   }
+}
+
+.form-item {
+  margin-top: 6px;
+}
 </style>
